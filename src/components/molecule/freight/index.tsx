9616c414eab072b1b629af85481b03e103fb8ea7
moises monteiro <moises.monteiro2077@hotmail.com>
2023-06-30 12:02:00 -0300

    Using mock-adapter

--- src/components/molecule/freight/index.tsx
+++ src/components/molecule/freight/index.tsx
@@ -3,52 +3,46 @@ import InputField from '@/components/atomic/inputs';
 import { useState } from 'react';
 import { B_forms } from '@/components/atomic/buttons';
 import axios from 'axios';
-import { useCounter } from '@/components/molecule/counter';
-// import { calculateFreight } from '@/components/api_extenal/calculatefreight';
+import MockAdapter from 'axios-mock-adapter';
 
+interface FreightLocale {
+  localidade: string;
+  uf: string;
+  bairro: string;
+}
 
-interface Freight{
-  valorpac: String;
-  prazopac: string;
-  valorsedex:string;
-  prazosedex:string;
-  localidade:string;
-  uf:string;
-
+interface FreightPrice {
+  valor: string;
+  prazo: string;
 }
 
 export default function Freight() {
-  const locale = {}
-  const details = {}
-
-
+  const [cep, setCep] = useState('');
+  const [freightLocale, setFreightLocale] = useState<FreightLocale | null>(null);
+  const [freightPrice, setFreightPrice] = useState<FreightPrice | null>(null);
+  const [loading, setLoading] = useState(false);
 
   const handleCepChange = (event) => {
     setCep(event.target.value);
   };
-
-  
 
   const handleCalculateFreight = async () => {
     if (cep) {
       setLoading(true);
 
       try {
-        // Obter informações de endereço com base no CEP usando a API ViaCEP
-        locale = {response} = await axios.get<Freight>(`https://viacep.com.br/ws/${cep}/json/`);
+        const response1 = await axios.get<FreightLocale>(`https://viacep.com.br/ws/${cep}/json/`);
+        setFreightLocale(response1.data);
 
-        // Simulação do cálculo de frete com base nas informações de endereço obtidas
-        datails = {details} = await axios.get<Freight>(`https://cepcerto.com/ws/json-frete/01153000/${cep}/500`);
-        
-        // const freight = calculateFreight(response.data);
-        // setFreightValue(freight);
+        // Fazer a solicitação para a API dos Correios para calcular o frete
 
-        // const{Prazopac,prazopac} = details.data;
-        // setpacprazo(`${prazopac}`);
+        const mock = new MockAdapter(axios);
+        const mockResponse = { valor: '25.00', prazo: '16 dias úteis' };
+        mock.onGet(`/frete?cepOrigem=01153000&cepDestino=${cep}&peso=500`).reply(200, mockResponse);
 
-        
+        const response2 = await axios.get<FreightPrice>(`/frete?cepOrigem=01153000&cepDestino=${cep}&peso=500`);
+        setFreightPrice(response2.data);
 
-        
       } catch (error) {
         console.error('Erro ao calcular frete:', error);
       }
@@ -57,15 +51,6 @@ export default function Freight() {
     }
   };
 
-  const calculateFreight = (address) => {
-    // Simulação do cálculo de frete com base nas informações de endereço
-    // Aqui você pode implementar a lógica real para o cálculo do frete
-    // Neste exemplo, estamos apenas retornando um valor fixo de R$ 10.00
-    return 0.00;
-  };
-
-  // const { count } = useCounter();
-  
   return (
     <div>
       <InputField
@@ -77,21 +62,19 @@ export default function Freight() {
         value={cep}
         onChange={handleCepChange}
       />
-      <B_forms onclick={handleCalculateFreight} size="small" name="freight"/>
+      <B_forms onclick={handleCalculateFreight} size="small" name="freight" />
 
       {loading && <p>Calculando frete...</p>}
       <div className='flex flex-row'>
-          <p className='font-bold'>Valor do frete:ㅤ</p>
-          {  <p> {data.uf} </p> } 
+        <p className='font-bold'>Valor do frete:ㅤ{freightPrice &&( <p className='font-light'>{freightPrice.valor}</p> )}</p>
+       
       </div>
       <div className='flex flex-row'>
           <p className='font-bold'>Enviar para:ㅤ</p>
-          {<p>{data.uf}</p>}
-
-          
+          {freightLocale &&(<p>{freightLocale?.localidade}, {freightLocale?.uf}.</p>)}
       </div>
-      {/* <p className='font-bold'>Quantidade:{count}</p> */}
     </div>
   );
 }
+
       
