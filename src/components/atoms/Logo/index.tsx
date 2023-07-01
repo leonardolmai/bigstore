2053163a115f361615ef8ca4fbc70bdaa1764e95
Leonardo Lima <se.leonardolima@gmail.com>
2023-07-01 16:23:14 -0300

    Update logo component to accept custom classes

--- src/components/atoms/Logo/index.tsx
+++ src/components/atoms/Logo/index.tsx
@@ -1,10 +1,13 @@
 import Image from 'next/image'
 import bigstoreLogo from '@/assets/bigstore-logo.png'
 import Link from 'next/link'
+import { ClassNameProps } from '@/types/componentClassName'
 
-export function Logo() {
+export function Logo({ className }: ClassNameProps) {
+  const logoClasses = `${className} shrink-0`
+
   return (
-    <Link href={'/'} className="shrink-0">
+    <Link href={'/'} className={logoClasses}>
       <Image src={bigstoreLogo} alt="Bigstore" height={60} priority={true} />
     </Link>
   )
