a6c414bd16e5a9fe6cd32184480a4fbf62900a25
Moises Monteiro <moises.monteiro2077@hotmail.com>
2023-07-11 07:30:57 -0300

    Create form company

--- src/components/organisms/company/product/index.tsx
+++ src/components/organisms/company/product/index.tsx
@@ -1,145 +1,180 @@
-'use client'
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function Formulario({ screens }) {
-  const [titulo, setTitulo] = useState('');
-  const [texto, setTexto] = useState('');
-  const [quantidade, setQuantidade] = useState(0);
-  const [preco, setPreco] = useState(0);
-  const [categoria, setCategoria] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState(0);
+  const [price, setPrice] = useState(0);
+  const [category, setCategory] = useState('');
+  const [images, setImages] = useState([]);
 
   const [alingForm, setAlingForm] = useState('');
-  const [forminput, setFormInput] = useState('');
-
-  const [photos, setPhotos] = useState([]);
+  const [formInput, setFormInput] = useState('');
 
   useEffect(() => {
     if (typeof window !== 'undefined') {
       if (screens.isLargeScreen) {
-        setAlingForm("w-[1145px]");
-        setFormInput('w-60');
+        setAlingForm('w-[1145px]');
+        setFormInput('w-[700px] h-[300px]');
       } else if (screens.isMediumScreen) {
-        setAlingForm("w-[880px]");
+        setAlingForm('w-[880px]');
+        setFormInput('w-[600px] h-[300px]');
+
       } else if (screens.isSmallScreen) {
-        setAlingForm("w-[666px]");
+        setAlingForm('w-[666px]');
+        setFormInput('w-[500px] h-[300px]');
       } else if (screens.isNanoScreen) {
-        setAlingForm("w-[380px]");
+        setAlingForm('w-[380px]');
+        setFormInput('w-[200px] h-[300px]');
       } else if (screens.isSmallNanoScreen) {
-        setAlingForm("w-[280px]")
+        setAlingForm('w-[280px]');
+        setFormInput('w-60');
       } else {
-        setAlingForm("w-[183px]")
+        setAlingForm('w-[183px]');
+        setFormInput('w-60');
       }
     }
-  }, [screens.isLargeScreen, screens.isMediumScreen, screens.isSmallScreen, screens.isNanoScreen, screens.isSmallNanoScreen]);
+  }, [
+    screens.isLargeScreen,
+    screens.isMediumScreen,
+    screens.isSmallScreen,
+    screens.isNanoScreen,
+    screens.isSmallNanoScreen,
+  ]);
 
   const handleFileChange = (event) => {
     const fileList = event.target.files;
     const selectedPhotos = Array.from(fileList);
-    setPhotos(selectedPhotos);
+    setImages(selectedPhotos);
   };
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    console.log(photos);
 
-    try {
-      const response = await axios.post('URL_DA_API', {
-        titulo,
-        texto,
-        quantidade,
-        preco,
-        categoria,
-      }, {
-        headers: {
-          Authorization: 'Token SEU_TOKEN',
-        },
-      });
+    if (typeof window !== 'undefined') {
+      event.preventDefault();
+      try {
+        const formData = new FormData();
+        formData.append('name', name);
+        formData.append('description', description);
+        formData.append('quantity', quantity);
+        formData.append('price', price);
+        formData.append('category', category);
+        images.forEach((image, index) => {
+          formData.append(`images[${index}].image`, image);
+        });
 
-      console.log('Resposta da API:', response.data);
-    } catch (error) {
-      console.error('Erro ao enviar formulário:', error);
+        const response = await axios.post('http://127.0.0.1:8000/api/products/', formData, {
+          headers: {
+            'x-company-cnpj': '00000000000000',
+            'Authorization': 'Bearer 3a8e363d71ca88ed56a45d931057756f1249381b',
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+
+        console.log('Formulário enviado:', response.data);
+      } catch (error) {
+        console.error('Erro ao enviar formulário:', error);
+      }
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className={`flex flex-col items-center ${alingForm} h-full`}>
-      <div className='flex flex-col items-start'>
-        <div className='gap-6 flex flex-row justify-center items-center'>
-          <div className='flex flex-col  w-48'>
-            <label htmlFor="titulo">Title:</label>
-            <input className="border-[#B1B1B1]  border-2 focus:outline-[#FEBD2F] rounded-lg"
+      <div className="flex flex-col  items-start">
+        <div className={`gap-6 flex ${screens.isSmallScreen === true ? "flex-row" : "flex-col"}  justify-center items-center`}>
+          <div className="flex flex-col w-48">
+            <label className="font-bold" htmlFor="name">
+              Title:
+            </label>
+            <input
+              className="border-[#B1B1B1] border-2 focus:outline-[#FEBD2F] rounded-lg"
               type="text"
-              id="titulo"
-              value={titulo}
-              onChange={(event) => setTitulo(event.target.value)}
+              id="name"
+              value={name}
+              onChange={(event) => setName(event.target.value)}
             />
           </div>
-          <div className='flex flex-col  w-48'>
-            <label htmlFor="Photo_List opacity-0 ">Photo List</label>
-            <div className='flex flex-1 justify-center cursor-pointer pt-1 bg-white border-[#B1B1B1]  active:bg-[#FEBD2F] focus:outline-[FEBD2F] w-24 rounded-lg'>
-              <p className='absolute cursor-pointer'>Photos</p>
-              <input className="bg-blue-500 text-white py-2 px-4 rounded-lg cursor-pointer" style={{ background: 'red' }}
-                type="file"
-                id="photos"
-                accept="image/*"
-                multiple
-                onChange={handleFileChange}
-              />
+          <div className={`flex flex-col  ${screens.isSmallScreen === true ? "" : "items-center"}`}>
+            <label className="font-bold" htmlFor="images">
+              Photo List:
+            </label>
+            <div>
+              <input type="file" id="images" accept="image/*" className={`flex flex-col  ${screens.isSmallScreen === true ? "w-60" : "w-[150px]"}`} multiple onChange={handleFileChange} />
             </div>
-
-
-
           </div>
         </div>
 
-        <div className='flex flex-col w-36'>
-          <label htmlFor="texto">Text:</label>
-          <textarea className={`resize-none border-[#B1B1B1]  w-[700px] h-[300px] border-2 focus:outline-[#FEBD2F] rounded-lg`}
-            id="texto"
-            value={texto}
-            onChange={(event) => setTexto(event.target.value)}
+        <div className="flex flex-col w-36">
+          <label className="font-bold" htmlFor="description">
+            Text:
+          </label>
+          <textarea
+            className={`resize-none border-[#B1B1B1] ${formInput} border-2 focus:outline-[#FEBD2F] rounded-lg`}
+            id="description"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
           ></textarea>
-        </div >
-        <div className='flex flex-row items-center gap-6'>
-          <div className='flex flex-col w-36'>
-            <label htmlFor="quantidade">Quantidade:</label>
-            <input className="border-[#B1B1B1] border-2 focus:outline-[#FEBD2F] rounded-lg"
+        </div>
+
+        <div className={`flex  ${screens.isSmallScreen === true ? "flex-row" : "flex-col"} items-center gap-6`}>
+          <div className="flex flex-col w-36">
+            <label className="font-bold" htmlFor="quantity">
+              Quantity:
+            </label>
+            <input
+              className="border-[#B1B1B1] border-2 focus:outline-[#FEBD2F] rounded-lg"
               type="number"
-              id="quantidade"
-              value={quantidade}
-              onChange={(event) => setQuantidade(event.target.value)}
+              id="quantity"
+              value={quantity}
+              onChange={(event) => setQuantity(event.target.value)}
             />
           </div>
 
-          <div className='flex flex-col'>
-            <label htmlFor="preco">Price:</label>
-            <input className="border-[#B1B1B1] border-2 focus:outline-[#FEBD2F] rounded-lg"
+          <div className="flex flex-col">
+            <label className="font-bold" htmlFor="price">
+              Price:
+            </label>
+            <input
+              className="border-[#B1B1B1] border-2 focus:outline-[#FEBD2F] rounded-lg"
               type="number"
               step="0.01"
-              id="preco"
-              value={preco}
-              onChange={(event) => setPreco(event.target.value)}
+              id="price"
+              value={price}
+              onChange={(event) => setPrice(event.target.value)}
             />
           </div>
-          <div className='flex flex-col'>
-            <label htmlFor="categoria">Category:</label>
-            <select className="border-[#B1B1B1] border-2 focus:outline-[#FEBD2F] rounded-lg"
-              id="categoria"
-              value={categoria}
-              onChange={(event) => setCategoria(event.target.value)}
+
+          <div className="flex flex-col">
+            <label className="font-bold" htmlFor="category">
+              Category:
+            </label>
+            <select
+              className="border-[#B1B1B1] border-2 focus:outline-[#FEBD2F] rounded-lg"
+              id="category"
+              value={category}
+              onChange={(event) => setCategory(event.target.value)}
             >
-              <option value="">Select an category</option>
-              <option value="categoria1">Category 1</option>
-              <option value="categoria2">Category 2</option>
-              <option value="categoria3">Category 3</option>
+              <option value="">Select a category</option>
+              <option value="electronics">Electronics</option>
+              <option value="clothing">Clothing</option>
+              <option value="home">Home</option>
+              <option value="health_beauty">Health Beauty</option>
+              <option value="sports_outdoors">Sports Outdoors</option>
+              <option value="books_movies_music">Books, Movies & Music</option>
+              <option value="toys_games">Toys & Games</option>
+              <option value="furniture_home_decor">Furniture Home Decor</option>
+              <option value="office_supplies">Office Supplies</option>
             </select>
           </div>
         </div>
-        <div className='flex flex-row justify-center items-center self-center m-2'>
-          <button className="p-1 pl-2 pr-2 bg-green-300 text-green-950 text-xl rounded-3xl" type="submit">Send</button>
+
+        <div className="flex flex-row justify-center items-center self-center m-2">
+          <button className="p-1 pl-2 pr-2 bg-green-300 text-green-950 text-xl rounded-3xl" type="submit">
+            Send
+          </button>
         </div>
       </div>
-    </form >
+    </form>
   );
 }
