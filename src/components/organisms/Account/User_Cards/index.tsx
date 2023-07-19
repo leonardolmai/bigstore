ebd55d696871260bfbb707305e4720603f43f339
Moises Monteiro <moises.monteiro2077@hotmail.com>
2023-07-19 00:22:49 -0300

    Created account Cards

--- src/components/organisms/Account/User_Cards/index.tsx
+++ src/components/organisms/Account/User_Cards/index.tsx
@@ -17,6 +17,27 @@ export default function User_Cards({ screens }) {
   const [alignbutton, setalignbutton] = useState('');
   const [aligncard, setaligncard] = useState('');
 
+  const [cards, setCards] = useState([]);
+  const [selectcard, setselectcards] = useState(null);
+  const [createdform, setcreatedform] = useState(false);
+  const [createdform2, setcreatedform2] = useState(false);
+  useEffect(() => {
+    const fetchCards = async () => {
+      try {
+        const response = await api.get('/cards/', {
+          headers: {
+            'Authorization': `Token ${getCookie('token')}`,
+          },
+        });
+        setCards(response.data);
+      } catch (error) {
+        console.error('Erro ao obter a lista de cartões:', error);
+      }
+    };
+    fetchCards();
+  }, []);
+
+
   useEffect(() => {
     if (typeof window !== 'undefined') {
       if (screens.isLargeScreen) {
@@ -65,6 +86,36 @@ export default function User_Cards({ screens }) {
     setImages(fileList);
   };
 
+  const handleCardSelection = (cardId) => {
+    const selectedCard = cards.find((card) => card.id === cardId);
+
+    if (selectedCard) {
+      setselectcards(selectedCard)
+      setName(selectedCard.name);
+      setDescription(selectedCard.description);
+      // E assim por diante para os outros campos do formulário
+    }
+  };
+
+  const toform = () => {
+    if (createdform === false) {
+      setcreatedform(true)
+    } else {
+      setcreatedform(false)
+    }
+
+  }
+
+  const toform2 = () => {
+    if (createdform2 === false) {
+      setcreatedform2(true)
+    } else {
+      setcreatedform2(false)
+    }
+
+  }
+
+
   const handleSubmit = async (event) => {
     if (typeof window !== 'undefined') {
       event.preventDefault();
@@ -103,44 +154,114 @@ export default function User_Cards({ screens }) {
     }
   };
 
+
   return (
     <form onSubmit={handleSubmit} className={`flex flex-col  items-center ${alingForm} h-full`}>
       <div className="flex flex-col  items-start ">
-        <div className={`gap-6 flex ${screens.isSmallScreen === true ? "flex-row" : "flex-col"}  justify-center items-center`}>
-          <div className="flex flex-col w-48">
-            <article className='flex flex-col items-center gap-6'>
-              <div className={`flex  ${aligncard} gap-2`}>
+        <div className={` gap-6 flex ${screens.isSmallScreen === true ? "flex-row" : "flex-col"}  justify-center items-center`}>
+          {createdform ? <div className='flex flex-col gap-6'>
+            <div className='flex flex-col'>
+              <label>nameCard</label>
+              <input type='number' className=' pl-1 min-w-full max-w-[550px]  rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input>
+
+            </div>
+
+            <div className='flex flex-col'>
+              <label>number Card</label>
+              <input type='text' className='  pl-1 min-w-full max-w-[550px]  rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input>
+            </div>
+
+            <div className='flex flex-col'>
+              <label>number Card</label>
+              <div className='flex flex-row gap-2'>
+                <input type='number' oninput="this.value = this.value.slice(0, 2)" placeholder="month" className='  pl-1  max-w-[75px] min-w-[65px] rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input><p>/</p>
+                <input type='number' pattern="[0-9]{4}" placeholder="year" className='  pl-1  max-w-[75px] min-w-[65px]    rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input>
+
+              </div>
+
+
+            </div>
+            <div className='flex flex-col'>
+              <label>CVC</label>
+              <input type='password' pattern="[0-9]{3}" className='  pl-1 min-w-full max-w-[550px]  rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input>
+            </div>
+            <div>
+              <button onClick={toform} className='no-select p-1 mt-2 w-full active:bg-white active:text-black text-white rounded-xl shadow-md cursor-pointer bg-[#000000]'>back</button>
+              <button onClick={toform} className='no-select p-1 mt-2 w-full active:bg-orange-500 rounded-xl shadow-md  cursor-pointer bg-[#FEBF2F]'>Create Card</button>
+            </div>
+
+          </div> : <>{createdform2 ? <div className='flex flex-col gap-6'>
+            <div className='flex flex-col'>
+              <label>nameCard2</label>
+              <input type='number' className=' pl-1 min-w-full max-w-[550px]  rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input>
+
+            </div>
+
+            <div className='flex flex-col'>
+              <label>number Card2</label>
+              <input type='text' className='  pl-1 min-w-full max-w-[550px]  rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input>
+            </div>
+
+            <div className='flex flex-col'>
+              <label>number Card2</label>
+              <div className='flex flex-row gap-2'>
+                <input type='number' oninput="this.value = this.value.slice(0, 2)" placeholder="month" className='  pl-1  max-w-[75px] min-w-[65px] rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input><p>/</p>
+                <input type='number' pattern="[0-9]{4}" placeholder="year" className='  pl-1  max-w-[75px] min-w-[65px]    rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input>
+
+              </div>
+
+
+            </div>
+            <div className='flex flex-col'>
+              <label>CVC2</label>
+              <input type='password' pattern="[0-9]{3}" className='  pl-1 min-w-full max-w-[550px]  rounded-lg shadow-md outline-2 focus:outline-[#FEDB2F]'></input>
+            </div>
+            <div>
+              <button onClick={toform2} className='no-select p-1 mt-2 w-full active:bg-white active:text-black text-white rounded-xl shadow-md cursor-pointer bg-[#000000]'>back</button>
+              <button onClick={toform2} className='no-select p-1 mt-2 w-full active:bg-orange-500 rounded-xl shadow-md  cursor-pointer bg-[#FEBF2F]'>Create Card</button>
+            </div>
+
+          </div> : <><div className="flex flex-col w-48">
+            <article className='flex flex-col items-center gap-6 '>
+              {selectcard == null ? <div className={`flex   ${aligncard} gap-2`}>
                 <div className='bg-gradient-to-r from-purple-600 to-blue-600 text-white w-[250px]  h-[130px] ml-10  p-2 items-start rounded-xl shadow shadow-black'>
-                  <p className='mt-1 mb-1'>Name: N N. N. N. </p>
-                  <p className='mt-1 mb-1'>Number: XXXX XXXX XXXX XXXX </p>
-                  <p className='mt-1 mb-1'>Validate: xx/xx</p>
-                  <p className='mt-1 mb-1'>CVC: ***</p>
+                  <p className='mt-1 mb-1 no-select'>Name: x </p>
+                  <p className='mt-1 mb-1 no-select'>Number: x </p>
+                  {/* {selectcard.number} */}
+                  <p className='mt-1 mb-1 no-select'>Validate: xx/xx</p>
+                  <p className='mt-1 mb-1 no-select'>CVC: ***</p>
                 </div>
                 <div className={`flex ${alignbutton}  gap-3   justify-center ml-2 items-center`}>
-                  <button className='bg-green-400 shadow rounded-lg p-1 w-14 hover:bg-green-800 active:bg-green-200'>Edit</button>
-                  <button className='bg-red-500 shadow  rounded-lg p-1 w-14 hover:bg-red-800 active:bg-red-200'>Delete</button>
+                  <button onClick={toform2} className='bg-green-400 shadow rounded-lg p-1 w-14 hover:bg-green-800 active:bg-green-200'>Edit</button>
+                  <button onClick={toform2} className='bg-red-500 shadow  rounded-lg p-1 w-14 hover:bg-red-800 active:bg-red-200'>Delete</button>
                 </div>
-              </div>
+              </div> : <div className='mb-32'></div>}
+
               <div>
                 <select id="category"
                   name="category"
-                  className="rounded-md border border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-primary-dark focus:outline-none focus:ring-primary-dark"
+                  className="no-select rounded-md border m-2 border-gray-300 bg-white px-3 py-2 shadow-sm focus:border-primary-dark focus:outline-none focus:ring-primary-dark"
                   defaultValue={''}
+                  onChange={(e) => handleCardSelection(e.target.value)}
                 >
                   <option value="" disabled>
                     List Cards
                   </option>
-
-                  <option value="Card 1">Card 1</option>
-                  <option value="Card 2">Card 2</option>
-
+                  {cards.map((card) => (
+                    <option key={card.id} value={card.id}>
+                      {card.name}
+                    </option>
+                  ))}
                 </select>
+
+                <button onClick={toform} className='no-select p-1 mt-2 w-full active:bg-orange-500 rounded-xl shadow cursor-pointer bg-[#FEBF2F]'>Create new Card</button>
               </div>
 
 
             </article>
 
-          </div>
+          </div></>}
+          </>}
         </div>
       </div>
     </form >
